b0c2075e942b8d84d4fb5d760d54f62dd633f5bb
Nabiha Batool <121992688+Nabihabatool@users.noreply.github.com>
2023-01-21 01:13:07 +0500

    Add files via upload

---
+++ static/2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+class datamember{
+	
+	public: 
+
+	 static int sr_no ;
+	 
+	 
+	 	datamember(){
+              	sr_no++;
+	}
+
+	 void gatdata();
+};
+int datamember::datamember()=0;
+
+void getdata::datamember(){
+	int sr_no=1;
+	int sr_no2=2;
+	int sr_no3=3;
+
+}
+
+int main() {
+	
+	datemember d;
+	datamember();
+	getdata();
+	
+	cout<<"I am object no 1"<<endl
+	cout<<"I am object no 2"<<endl;
+	cout<<"I am object no 3";
+	
+	
+	
+	
+	return 0;
+}
